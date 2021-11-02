53e9f50199ef50df7198aac3585255ab55291f15
imamhossain94 <imamagun94@gmail.com>
2021-11-02 12:10:31 +0600

    feat: script completed

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-import camelot as cam
-from requests import get
-import tabula
+from PIL import Image
+from pytesseract import pytesseract
+
+path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+image_path = r"table.png"
+
+img = Image.open(image_path)
+
+pytesseract.tesseract_cmd = path_to_tesseract
+
+text = pytesseract.image_to_string(img)
+# " ".join(text.split())
+# print(text[:-1])
+
+list_x = text.split('\n')
+list_y = []
+for x in list_x:
+    if x.strip() not in "      ":
+        # print(x.strip())
+        list_y.append(x.strip())
+
+# print(list_y)
+
+header = list_y[0].split(' ')
+data = list_y[1:]
+
+values = []
+for d in data:
+    values.append(d.split(' '))
+
+finalData = {
+    "header": header,
+    "values": values
+}
 
 
-def extract_table_from_pdf():
-    while True:
-        try:
-            f = open("table.pdf", 'wb')
-            f.write(
-                get("https://www.southalabama.edu/mathstat/personal_pages/mulekar/st550/Krishnakumar.pdf", ).content)
-            f.close()
-
-            table = cam.read_pdf("table.pdf", pages='3', flavor='stream')
-            # table[0].df[[5, 6]].to_excel("camelot_demo.xls")
-            print(table[0])
-
-            break
-        except Exception as e:
-            print(e)
-            raise
-
-
-extract_table_from_pdf()
+print(finalData)
